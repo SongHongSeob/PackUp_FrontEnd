61b410540200f7b5593a469dea3454c6d9f0be78
송홍섭 <yahoo505@naver.com>
2025-08-25 15:49:54 +0900

    fix: /api/api -> /api로 수정

--- src/pages/AuthPage/components/LoginForm.tsx
+++ src/pages/AuthPage/components/LoginForm.tsx
@@ -35,7 +35,7 @@ const LoginForm = () => {
                 userPw: data.password
             };
     
-            const response = await fetch('http://3.35.147.68:8080/api/api/lgn/login', {
+            const response = await fetch('http://3.35.147.68:8080/api/lgn/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
